4cb2ff0814acd51963e3b693a5201cd683948e22
Franklin Shih <96925425+fshih1997@users.noreply.github.com>
2022-08-25 14:44:20 -0700

    fixed error
    fix number of parameter error

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # Serve model as a flask application
 
-import pickle
 import numpy as np
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
+import pickle
 
 model = None
 app = Flask(__name__)
@@ -17,19 +17,28 @@ def load_model():
 
 @app.route('/')
 def home_endpoint():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @app.route('/predict', methods=['POST'])
-def get_prediction():
+def predict():
     # Works only for a single sample
     if request.method == 'POST':
-        data = request.get_json()  # Get data posted as a json
-        data = np.array(data)[np.newaxis, :]  # converts shape from (4,) to (1, 4)
-        prediction = model.predict(data)  # runs globally loaded model on the data
-    return str(prediction[0])
+        int_features = [int(x) for x in request.form.values()]
+        # print(request.form.values())
+        final_features = np.array(int_features)[np.newaxis, :]
+        prediction = model.predict(final_features)
+
+        output = round(prediction[0],2)
+
+        return render_template('index.html', prediction_text='Iris should be type {}'.format(output))
+
+        # data = request.get_json()  # Get data posted as a json
+        # data = np.array(data)[np.newaxis, :]  # converts shape from (4,) to (1, 4)
+        # prediction = model.predict(data)  # runs globally loaded model on the data
+    # return str(prediction[0])
 
 
 if __name__ == '__main__':
     load_model()  # load model at the beginning once only
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
